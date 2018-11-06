5c002829435df5546cccafe2390bdd7bbdd0af6b
Francisco Zurita <zurit92@gmail.com>
2018-11-06 11:24:54 +0100

    Component name changed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <KustomerChat msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import KustomerChat from './components/KustomerChat.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    KustomerChat
   }
 }
 </script>
